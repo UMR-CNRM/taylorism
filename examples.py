88e6e87cb74352e68ef5ccf2092c31c64ceed099
Alexandre Mary <alexandre.mary@meteo.fr>
2016-03-04 14:34:05 +0100

    New taylorism package

---
+++ examples.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Basic examples of how to use the module.
+
+A more advanced example of use can be found in **epygram**'s epy_conv.py tool.
+"""
+
+from __future__ import absolute_import, print_function
+
+import time
+
+from taylorism import Worker, run_as_server
+from taylorism.schedulers import MaxThreadsScheduler
+
+
+class Sleeper(Worker):
+    """
+    Sample worker for tutorial or debugging purpose, that sleeps a given time.
+
+    The over-loading of __init__() is not mandatory, but a possibility.
+    """
+
+    _footprint = dict(
+        info="Sleeps.",
+        attr=dict(
+            sleeping_time=dict(
+                info="Sleeping time in s.",
+                type=float,
+                values=[1, 2, 3, 5, 10, 15, 30, 60]),
+            wakeup_sentence=dict(
+                info="What to say after sleep.",
+                type=str,
+                optional=True,
+                access='rwx',
+                default=None)
+            )
+        )
+
+    def __init__(self, *args, **kwargs):
+        super(Sleeper, self).__init__(*args, **kwargs)
+        if self.wakeup_sentence is None:
+            self.wakeup_sentence = 'Hello !'
+
+    def _task(self):
+        time.sleep(self.sleeping_time)
+        return ' '.join([self.wakeup_sentence, "Woke up after", str(self.sleeping_time), "s sleep."])
+
+
+def example_program(verbose=True):
+    """Example: how to run and control the Boss."""
+
+    boss = run_as_server(common_instructions={},
+                         individual_instructions={'sleeping_time':[5, 10, 2, 1]},
+                         scheduler=MaxThreadsScheduler(max_threads=3),
+                         verbose=verbose)
+    time.sleep(6)
+    print(boss.get_report())
+    boss.set_instructions({}, individual_instructions={'sleeping_time':[3, ]})
+    boss.wait_till_finished()
+    report = boss.get_report()
+    for r in report['workers_report']:
+        print(r)
---
+++ schedulers.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Contains classes for Schedulers.
+
+Among a set of instructions to be passed to a Worker, and according to its own
+criteria, the Scheduler determine at the current moment the ones that can be
+launched right now simultaneously, and those that must be delayed.
+
+A scheduler hence basically has one method:
+launchable(pending_instructions, workers, report).
+
+Its parameters (constant among execution) can be attributed in its constructor.
+Other quantities, variables among execution, must be available within
+*workers* (work being done) and *report* (work done).
+
+A set of basic schedulers is given.
+"""
+
+from __future__ import absolute_import, print_function
+
+
+class BaseScheduler(object):
+    """Abstract class."""
+    def launchable(self, pending_instructions, workers, report):
+        """
+        Split *pending_instructions* into "launchable" and "not_yet_launchable"
+        instructions according to the scheduler own rules.
+
+        For that purpose and in a generic manner, the scheduler may need:\n
+        - *pending_instructions*: todo
+        - *workers*: being done
+        - *report*: done.
+        """
+        raise NotImplementedError('launchable() method must be implemented in \
+                                   inheritant classes. (BaseScheduler is abstract).')
+
+
+class LaxistScheduler(BaseScheduler):
+    """No sorting is done !"""
+    def launchable(self, pending_instructions, workers, report):
+        return pending_instructions
+
+
+class MaxThreadsScheduler(BaseScheduler):
+    """
+    A basic scheduler that dequeue the pending list as long as a maximum number
+    of simultaneous tasks (*max_threads*) is not reached.
+    """
+    import multiprocessing as mpc
+
+    def __init__(self, max_threads=mpc.cpu_count() / 2):
+        """*max_threads* to be launched simultaneously."""
+        self.max_threads = max_threads
+
+    def launchable(self, pending_instructions, workers, report):
+        available_threads = self.max_threads - len(workers)
+        launchable = pending_instructions[0:max(available_threads, 0)]
+        not_yet_launchable = pending_instructions[max(available_threads, 0):]
+
+        return (launchable, not_yet_launchable)
+
+
+class MaxMemoryScheduler(BaseScheduler):
+    """
+    A basic scheduler that dequeue the pending list as long as a critical memory
+    level (according to 'memory' element of workers instructions (in MB) and
+    total system memory) is not reached.
+    """
+
+    def __init__(self, max_memory_percentage=0.75, total_system_memory='compute'):
+        """
+        *max_memory_percentage*: max memory level as a percentage of the total
+        system memory.
+        *total_system_memory*: total system memory in MB;
+        if 'compute', computed (Unix only).
+        """
+        import os
+
+        if total_system_memory == 'compute':
+            total_system_memory = os.sysconf('SC_PAGE_SIZE') * os.sysconf('SC_PHYS_PAGES')
+            total_system_memory = float(total_system_memory) / (1024 ** 2.)
+        self.max_memory = max_memory_percentage * total_system_memory
+
+    def launchable(self, pending_instructions, workers, report):
+        assert all([hasattr(w, 'memory') for w in workers.values()])
+        used_memory = sum([w.memory for w in workers.values()])
+        launchable = []
+        not_yet_launchable = []
+        for instructions in pending_instructions:
+            if used_memory + instructions['memory'] < self.max_memory:
+                launchable.append(instructions)
+                used_memory += instructions['memory']
+            else:
+                not_yet_launchable.append(instructions)
+
+        return (launchable, not_yet_launchable)
