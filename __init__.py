5583cae713ab6fc754376c32083237f7a9e8df70
Louis-Francois Meunier <louis-francois.meunier@meteo.fr>
2016-08-11 10:05:39 +0100

    Update the version number 0.10.0 -> 0.10.1 (1.0.3 for taylorism)

--- __init__.py
+++ __init__.py
@@ -91,7 +91,7 @@ taylorism_log = footprints.loggers.getLogger(__name__)
 #: timeout when polling for a Queue/Pipe communication
 communications_timeout = 0.01
 
-__version__ = '1.0.2'
+__version__ = '1.0.3'
 
 #################
 ### FUNCTIONS ###
