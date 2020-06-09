1e4f0caa1333ccbface3cb762461a17109d812dc
Louis-Francois Meunier <louis-francois.meunier@meteo.fr>
2020-06-09 15:40:17 +0200

    Bump version numbers

--- __init__.py
+++ __init__.py
@@ -100,7 +100,7 @@ taylorism_log = loggers.getLogger(__name__)
 # : timeout when polling for a Queue/Pipe communication
 communications_timeout = 0.01
 
-__version__ = '1.0.13'
+__version__ = '1.0.14'
 
 
 # FUNCTIONS
