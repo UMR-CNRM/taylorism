442c1505bb5613dd0f0c87ee039c0f17cda87b70
Louis-Francois Meunier <louis-francois.meunier@meteo.fr>
2022-11-14 15:50:32 +0100

    Increment version numbers

--- __init__.py
+++ __init__.py
@@ -99,7 +99,7 @@ taylorism_log = loggers.getLogger(__name__)
 # : timeout when polling for a Queue/Pipe communication
 communications_timeout = 0.01
 
-__version__ = '1.0.15'
+__version__ = '1.0.16'
 
 
 # FUNCTIONS
